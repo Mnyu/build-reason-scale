78233d925d16238977f5d8b358d38b13b1254e0a
Abhimanyu <mail2abhimanyu.gupta@gmail.com>
2026-01-02 18:51:32 +0530

    signup component ui

---
+++ app/auth/layout.tsx
@@ -0,0 +1,18 @@
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowLeftIcon } from "lucide-react";
+import Link from "next/link";
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="absolute top-5 left-5">
+        <Link href="/" className={buttonVariants({ variant: "secondary" })}>
+          <ArrowLeftIcon className="size-4" />
+          Go Back
+        </Link>
+      </div>
+      <div className="w-full max-w-md mx-auto">{children}</div>
+    </div>
+  );
+};
+export default AuthLayout;
--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,4 +1,73 @@
+"use client";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signupSchema } from "@/app/schemas/auth";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 const SignupPage = () => {
-  return <div>SignupPage</div>;
+  const form = useForm({
+    resolver: zodResolver(signupSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = () => {
+    console.log("handleSubmit");
+  };
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Sign Up</CardTitle>
+        <CardDescription>Create an account to get started</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FieldGroup className="gap-y-4">
+            <Controller
+              name="name"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel>Full Name</FieldLabel>
+                  <Input placeholder="John Doe" {...field} aria-invalid={fieldState.invalid} />
+                  {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                </Field>
+              )}
+            />
+            <Controller
+              name="email"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel>Email</FieldLabel>
+                  <Input type="email" placeholder="john@doe.com" {...field} aria-invalid={fieldState.invalid} />
+                  {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                </Field>
+              )}
+            />
+            <Controller
+              name="password"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel>Password</FieldLabel>
+                  <Input type="password" placeholder="********" {...field} aria-invalid={fieldState.invalid} />
+                  {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                </Field>
+              )}
+            />
+            <Button>Sign up</Button>
+          </FieldGroup>
+        </form>
+      </CardContent>
+    </Card>
+  );
 };
 export default SignupPage;
